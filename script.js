5f4da6b2c6bcd60c966a20616e2129e21cafad3c
Tenish <tenish02@gmail.com>
2020-07-30 16:08:50 +0800

    your updated message

--- script.js
+++ script.js
@@ -148,20 +148,3 @@ let i = 0
 	// 	else 
 	// 		console.log(i)
 	// }
-
-	for (let i = 1; i <= 100; i++){
-		let output = "";
-
-		if(i % 3 == 0) {
-			output += "Fizz"
-		}
-
-		if(i % 5 == 0) {
-			output += "Buzz"
-		}
-
-		if(output == "") {
-			output = i
-		}
-		console.log(output)
-	}
